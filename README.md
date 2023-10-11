9e4a6b125f741449dadc36d9835c6aa1bf499202
Jonathan W <wuntujoy@gmail.com>
2023-10-11 22:13:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WeatherApp | KOTLIN
+
+### ID
+### Cara Menggunakan App
+- Nyalakan Internet
+- Nyalakan Lokasi
+- Jalankan Aplikasi
+
+### EN
+### HOW TO USE THE APP
+- Turn On Internet
+- Turn On Location
+- Run the App
---
+++ app/src/main/java/com/example/weatherapp/Utils.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp
+
+class Utils {
+    companion object{
+
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = "f7039b99de808f6c99378a94461b423c"
+         const val PERMISSION_REQUEST_CODE = 123
+
+         const val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
+
+
+
+
+
+    }
+}
